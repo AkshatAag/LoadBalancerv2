610f5ab21172c382ff5d19e65e1ae91b91ea8fcf
akshat.agrawal <agrawalakshat049@gmail.com>
2023-06-28 12:32:40 +0530

    latency brought down to 150ms

--- src/main/java/com/example/loadbalancer/repository/ConversationsRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.loadbalancer.repository;
-
-import com.example.loadbalancer.entity.ConversationDetails;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ConversationsRepo extends MongoRepository<ConversationDetails, String> {
-}
--- src/main/java/com/example/loadbalancer/service/Service.java
+++ src/main/java/com/example/loadbalancer/service/Service.java
@@ -1,86 +1,84 @@
 package com.example.loadbalancer.service;
 
 import com.example.loadbalancer.entity.*;
-import com.example.loadbalancer.repository.CallRepo;
-import com.example.loadbalancer.repository.ConversationsRepo;
 import com.example.loadbalancer.repository.LoadRedis;
-import com.example.loadbalancer.repository.MediaLayerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+
+import static com.example.loadbalancer.utils.Utils.LEAST_CONNECTIONS;
+import static com.example.loadbalancer.utils.Utils.ROUND_ROBIN;
 
 @org.springframework.stereotype.Service
 public class Service {
 
-    private static final int LEAST_CONNECTIONS = 1;
-    private static final int ROUND_ROBIN = 2;
+
     private final LoadRedis loadRedis;
-    private final CallRepo callRepo;
-    private final MediaLayerRepo mediaLayerRepo;
-    private final ConversationsRepo conversationsRepo;
+    private final MongoTemplate mongoTemplate;
 
     @Autowired
-    public Service(LoadRedis loadRedis, CallRepo callRepo, MediaLayerRepo mediaLayerRepo, ConversationsRepo conversationsRepo) {
+    public Service(LoadRedis loadRedis, MongoTemplate mongoTemplate) {
         this.loadRedis = loadRedis;
-        this.callRepo = callRepo;
-        this.mediaLayerRepo = mediaLayerRepo;
-        this.conversationsRepo = conversationsRepo;
+        this.mongoTemplate = mongoTemplate;
+    }
+    public void deleteById(String id,Class<?> entityClass) {
+        mongoTemplate.remove(Query.query(Criteria.where("_id").is(id)), entityClass);
     }
 
     @Scheduled(fixedDelay = 5, initialDelay = 0, timeUnit = TimeUnit.SECONDS)
-    public void updateMongoDuration() {
-        List<MediaLayer> mediaLayerList = mediaLayerRepo.findAll();
+    public void refreshDatabaseMongo() {
+        List<MediaLayer> mediaLayerList = mongoTemplate.findAll(MediaLayer.class);
         for (MediaLayer mediaLayer : mediaLayerList) {
             long curTime = System.currentTimeMillis();
             long duration = mediaLayer.getDuration() + (curTime - mediaLayer.getLastModified()) * mediaLayer.getNumberOfCalls();
             mediaLayer.setDuration(duration);
             mediaLayer.setLastModified(curTime);
             mediaLayer.setRatio();
+            mongoTemplate.save(mediaLayer);
         }
-        mediaLayerRepo.saveAll(mediaLayerList);
     }
 
     public String processEventControlLayer(CallFromControlLayer callFromControlLayer, int alg) {
 
         String legId = callFromControlLayer.getLegId();
         String conversationId = callFromControlLayer.getConversationId();
-        Optional<ConversationDetails> optionalConversationDetails = conversationsRepo.findById(conversationId);
-
+        ConversationDetails conversationDetails = mongoTemplate.findById(conversationId, ConversationDetails.class);
         MediaLayer destinationMediaLayer;
         String mediaLayerNumber;
 
-        if (optionalConversationDetails.isPresent()) { //if this conversation already has an ongoing media layer assigned to it
-            ConversationDetails conversationDetails = optionalConversationDetails.get();
-            Optional<MediaLayer> optionalMediaLayer = mediaLayerRepo.findById(conversationDetails.getMediaLayerNumber());
+        if (conversationDetails != null) { //if this conversation already has an ongoing media layer assigned to it
+            destinationMediaLayer = mongoTemplate.findById(conversationDetails.getMediaLayerNumber(), MediaLayer.class);
 
-            if (optionalMediaLayer.isPresent()) {
-                destinationMediaLayer = optionalMediaLayer.get();
-            } else {
+            if (destinationMediaLayer == null) {
                 return "Conversation is going on but unable to find corresponding media layer";
             }
 
             mediaLayerNumber = conversationDetails.getMediaLayerNumber();
             conversationDetails.incrementLegCount();
 
-            new Thread(() -> conversationsRepo.save(conversationDetails)).start();
+            new Thread(() -> mongoTemplate.save(conversationDetails)).start();
 
             System.out.println("Call was added to ongoing conversation");
         } else {
             destinationMediaLayer = getLeastLoaded(alg);
+            assert destinationMediaLayer != null;
             mediaLayerNumber = destinationMediaLayer.getLayerNumber();
 
-            new Thread(() -> conversationsRepo.save(new ConversationDetails(1, mediaLayerNumber, conversationId))).start();
+            new Thread(() -> mongoTemplate.save(new ConversationDetails(1, mediaLayerNumber, conversationId))).start();
             System.out.println("Call was added to the least loaded server");
         }
 
         destinationMediaLayer.updateDetails();
         new Thread(() -> {
-            callRepo.save(new Call(legId, conversationId, mediaLayerNumber, System.currentTimeMillis()));
+            mongoTemplate.save(new Call(legId, conversationId, mediaLayerNumber, System.currentTimeMillis()));
             loadRedis.setConversationId(legId, conversationId);
-            mediaLayerRepo.save(destinationMediaLayer);
+            mongoTemplate.save(destinationMediaLayer);
         }).start();
 
         return "Send the call to media layer number : " + destinationMediaLayer.getLayerNumber();
@@ -89,9 +87,16 @@ public class Service {
     private MediaLayer getLeastLoaded(int alg) {
         switch (alg) {
             case LEAST_CONNECTIONS:
-                return mediaLayerRepo.findDestinationLeastConnections();
+                Query queryLeastConnections = new Query(Criteria.where("faulty").is(false))
+                        .with(Sort.by(Sort.Direction.ASC, "ratio").and(Sort.by(Sort.Direction.ASC, "duration")))
+                        .limit(1);
+                return mongoTemplate.findOne(queryLeastConnections, MediaLayer.class);
+
             case ROUND_ROBIN:
-                return mediaLayerRepo.findDestinationRoundRobin();
+                Query queryRoundRobin = new Query(Criteria.where("faulty").is(false))
+                        .with(Sort.by(Sort.Direction.ASC, "latestCallTimeStamp"))
+                        .limit(1);
+                return mongoTemplate.findOne(queryRoundRobin, MediaLayer.class);
             default:
                 return null;
         }
@@ -109,31 +114,27 @@ public class Service {
 
     private boolean handleHangupEvent(EventFromMediaLayer event) {
 
-        Optional<Call> optionalCall = callRepo.findById(event.getCoreUUID());
-        Call currentCall;
-        if (optionalCall.isPresent()) {
-            currentCall = optionalCall.get();
-        } else {
+        Call currentCall = mongoTemplate.findById(event.getCoreUUID(), Call.class);
+        if (currentCall == null) {
             return false;
         }
         String conversationId = currentCall.getConversationId();
         String legId = currentCall.getCallId();
-        Optional<ConversationDetails> optionalConversationDetails = conversationsRepo.findById(conversationId);
+        ConversationDetails conversationDetails = mongoTemplate.findById(conversationId, ConversationDetails.class);
 
-        if (optionalConversationDetails.isPresent()) {
-            ConversationDetails conversationDetails = optionalConversationDetails.get();
+        if (conversationDetails != null) {
 
             new Thread(() -> {
-                callRepo.deleteById(legId);
+                deleteById(legId,Call.class);
                 loadRedis.remove(legId);
             }).start();
             conversationDetails.decrementLegCount();
 
             new Thread(() -> {
                 if (conversationDetails.getLegCount() == 0) {
-                    conversationsRepo.deleteById(conversationId);
+                    deleteById(conversationId,ConversationDetails.class);
                 } else {
-                    conversationsRepo.save(conversationDetails);
+                    mongoTemplate.save(conversationDetails);
                 }
             }).start();
 
@@ -146,16 +147,12 @@ public class Service {
     }
 
     private Boolean updateMediaLayerDatabaseHangupEvent(ConversationDetails conversationDetails, Call currentCall) {
-        Optional<MediaLayer> optionalMediaLayer = mediaLayerRepo.findById(conversationDetails.getMediaLayerNumber());
-        if (optionalMediaLayer.isPresent()) {
+        MediaLayer mediaLayer = mongoTemplate.findById(conversationDetails.getMediaLayerNumber(), MediaLayer.class);
+        if (mediaLayer!=null) {
             new Thread(() -> {
-                MediaLayer mediaLayer = optionalMediaLayer.get();
-
-
                 mediaLayer.decreaseDuration(System.currentTimeMillis(), currentCall.getTimeStamp());
                 mediaLayer.decrementNumberOfCalls();
-
-                mediaLayerRepo.save(mediaLayer);
+                mongoTemplate.save(mediaLayer);
             }).start();
             return true;
         } else {
@@ -164,29 +161,25 @@ public class Service {
     }
 
     public String addNewMediaLayer(MediaLayer mediaLayer) {
-        mediaLayerRepo.save(mediaLayer);
+        mongoTemplate.save(mediaLayer);
         return "NEW Media Layer was added to Mongo";
     }
 
 
     public String setServerStatus(String layerNumber, String color) {
-        Optional<MediaLayer> optionalMediaLayer = mediaLayerRepo.findById(layerNumber);
-        if (optionalMediaLayer.isPresent()) {
-            MediaLayer mediaLayer = optionalMediaLayer.get();
-
+        MediaLayer mediaLayer = mongoTemplate.findById(layerNumber, MediaLayer.class);
+        if (mediaLayer!=null) {
             mediaLayer.setStatus(color);
-            mediaLayerRepo.save(mediaLayer);
+            mongoTemplate.save(mediaLayer);
         }
         return "Server number " + layerNumber + " status was changed to " + color;
     }
 
     public String setFaultyStatus(String layerNumber, boolean status) {
-        Optional<MediaLayer> optionalMediaLayer = mediaLayerRepo.findById(layerNumber);
-        if (optionalMediaLayer.isPresent()) {
-            MediaLayer mediaLayer = optionalMediaLayer.get();
-
+        MediaLayer mediaLayer = mongoTemplate.findById(layerNumber, MediaLayer.class);
+        if (mediaLayer!=null) {
             mediaLayer.setFaulty(status);
-            mediaLayerRepo.save(mediaLayer);
+            mongoTemplate.save(mediaLayer);
         }
         return "Server number " + layerNumber + " faulty status was changed to " + status;
     }
